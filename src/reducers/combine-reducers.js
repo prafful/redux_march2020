28944d5dfcabe60e94e78c6f7366c426fe8d27ba
Prafful Daga <prraful@gmail.com>
2020-03-27 16:53:09 +0530

    redux - axios is returning promise but must return an array

--- src/reducers/combine-reducers.js
+++ src/reducers/combine-reducers.js
@@ -4,13 +4,15 @@ import taxiService from "./reducer-taxiservice";
 import taxiNameClickedAndActionReceived from "./reducer-taxiname_clicked";
 import taxiNameDoubleClickedAndActionReceived from "./reducer-taxiname_doubleclicked";
 import taxiLikeChangeAndActionReceived from "./reducer-taxilikechange";
+import getRemoteUsers from "./reducer-remoteuser";
 
 const allTaxiReducers = combineReducers({
     alltaxinames: taxiData,
     alltaxiservices: taxiService,
     taxiClicked:taxiNameClickedAndActionReceived,
     taxiDoubleClicked: taxiNameDoubleClickedAndActionReceived,
-    taxiLikeChange: taxiLikeChangeAndActionReceived
+    taxiLikeChange: taxiLikeChangeAndActionReceived,
+    remoteUsers: getRemoteUsers
 })
 
 export default allTaxiReducers
